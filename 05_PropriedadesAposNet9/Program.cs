99c86e30764f0c25518914bc5c50bd2f36da2243
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-12 20:30:30 -0300

    Encapsulamento com propriedades a partir da versão doteNet 9

---
+++ 05_PropriedadesAposNet9/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_PropriedadesAposNet9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conta conta = new Conta();
+            Conta conta = new Conta("999-88");
+
+            //conta.Nome = "Thiago Silva";
+            conta.Nome = "Thiago Silva" ; //Utilizando metodo acessador
+
+            //Não é possível atribuir valor ao campo numero, pois ele é privado
+            //conta.Numero = "999-88";
+
+
+            //conta.saldo = -1000.00m;  --Erro, pois saldo é privado
+            conta.Depositar(-1000.00m) ; // m de valor monetário
+
+            //Utilizando metodo acessador
+            conta.Saldo = 1000.00m; // menbro publico (propriedade) - Atribuição de valor
+
+            //Console.WriteLine(conta.getSaldo():c2);
+            conta.ImprimirSaldo();
+
+
+            while (true)
+            {
+                Console.WriteLine("Informe a Operação: [D] - Depositar, [S] - Sacar ou [E] - Sair");
+                string operacao = Console.ReadLine(); // Colocar .ToUpper() para evitar erro de digitação (Letras maiusculas)
+
+                if (operacao.ToUpper() == "D")
+                {
+                    Console.WriteLine("Informe o valor do Depósito");
+                    decimal valorDepositado = decimal.Parse(Console.ReadLine());
+                    conta.Depositar(valorDepositado);
+                    conta.ImprimirSaldo();
+                }
+                else if (operacao.ToUpper() == "S")
+                {
+                    Console.WriteLine("Informe o valor para Saque");
+                    decimal valorSaque = decimal.Parse(Console.ReadLine());
+                    conta.Sacar(valorSaque);
+                    conta.ImprimirSaldo();
+                }
+                else if (operacao.ToUpper() == "E")
+                {
+                    Console.WriteLine($"Conta: {conta.Numero} Nome: {conta.Nome} Saldo: {conta.Saldo:c2}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Informe apenas as letras [D] para depositar, [S] para Sacar e [E] para Sair");
+                }
+            }
+        }
+
+        public class Conta
+        {
+            //private string numero;
+
+            public string Numero { get; }
+            
+
+            //private string nome;
+
+            public string Nome { get; set; } //Propriedade autoimplementada
+
+            /*public string getNome() //metodo acessador
+            {
+                return nome; 
+            }
+            public void setNome(string nome) //metodo acessador
+            {
+                this.nome = nome; //This.nome referencia o campo da classe, já o nome sem o this referencia o parametro do metodo
+            }*/
+
+            /*public string getNumero()
+            {
+                return numero; 
+            }*/
+            public Conta(string numero) //Construtor é executado apenas uma vez quando o objeto é instanciado
+            {
+                this.Numero = numero;
+            }
+
+            //Transformar o campo Saldo em privado
+            //public decimal Saldo;
+            private decimal saldo;
+
+            //declaração da propriedade 
+            public decimal Saldo
+            {
+                get { return saldo; }//retorna o valor do campo saldo
+                set
+                {
+                    this.Depositar(value); //value é uma palavra reservada que representa o valor que está sendo atribuído a propriedade
+                }
+            }
+
+            //metodos acessadores
+            /*public decimal getSaldo()
+            {
+                return saldo;
+            }
+
+            public void setSaldo(decimal saldo)
+            {
+                this.Depositar(saldo);
+            }*/
+
+            public void Depositar(decimal valor)
+            {
+                if (valor > 0 )
+                    saldo += valor;
+                else
+                    Console.WriteLine("Valor de depósito incorreto, deve ser maior do que zero");
+
+            }
+            public void Sacar(decimal valor)
+            {
+                if (valor > 0)
+                {
+                    saldo -= valor;
+                }
+                else
+                {
+                    Console.WriteLine("Valor de saque incorreto, deve ser maior do que zero");
+                }
+
+            }
+
+            public void ImprimirSaldo()
+            {
+                Console.WriteLine($"Saldo atual da conta é {saldo:C2}");
+            }
+        }
+    }
+}
