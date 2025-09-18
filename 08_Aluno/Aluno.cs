ad341fe1f360985aabeeafc13df140e1fbfb096f
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-18 19:25:52 -0300

    criação da classe Aluno e propriedades em C#, aplicando encapsulamento e cálculo de propriedades derivadas.

--- 08_Aluno/Aluno.cs
+++ 08_Aluno/Aluno.cs
@@ -9,30 +9,57 @@ namespace _08_Aluno
     internal class Aluno
     {
         private string nome;
-        public int idade;
-        public double nota1, nota2;
-        public double media;
-        public string situacao;
+        private int idade;
+        private double nota1, nota2;
+        private double media;
+        private string situacao;
 
-        public double CalcularMedia(double nota1, double nota2)
+        public Aluno(string nome, int idade, double nota1, double nota2)
         {
-            media = (nota1 + nota2) / 2;
-            return media;
+            this.nome = nome;
+            this.idade = idade;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
         }
-        public string VerificarSituacao()
+        public string Nome
         {
-            if(media >= 6)
-            {
-                situacao = "Aprovado";
-            }
-            else
-            {
-                situacao = "Reprovado";
-            }
-            return situacao;
+            get { return nome; } 
+        }
+        public int Idade
+        {
+            get { return idade; }
+            set { idade = value; }
+        }
+        public double Nota1
+        {
+            get { return nota1; }
+            set { nota1 = value; }
+
+        }
+        public double Nota2
+        {
+            get { return nota2; }
+            set { nota2 = value; }
         }
 
+        public double Media
+        {
+            get { return(nota1 + nota2)/ 2.0 ; }
+        }
 
-
+        public string Situacao
+        {
+            get
+            {
+                if (Media >= 6)
+                {
+                    return "Aprovado";
+                }
+                else
+                {
+                    return "Reprovado";
+                }
+            }
+        }
     }
 }
--- 08_Aluno/Program.cs
+++ 08_Aluno/Program.cs
@@ -10,6 +10,23 @@ namespace _08_Aluno
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Digite o nome do aluno:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite a idade do aluno:");
+            int idade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a primeira nota do aluno:");
+            double nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a segunda nota do aluno:");
+            double nota2 = double.Parse(Console.ReadLine());
+            
+            Aluno aluno = new Aluno(nome, idade, nota1, nota2);
+            Console.WriteLine($"Nome: {aluno.Nome}");
+            Console.WriteLine($"Idade: {aluno.Idade}");
+            Console.WriteLine($"Nota 1: {aluno.Nota1}");
+            Console.WriteLine($"Nota 2: {aluno.Nota2}");
+            Console.WriteLine($"Média: {aluno.Media}");
+            Console.WriteLine($"Situação: {aluno.Situacao}");
         }
     }
 }
