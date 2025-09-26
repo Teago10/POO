b8d9630e49ca9b6f497c834608b7fd1aa5891069
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-25 21:47:12 -0300

    Coleções. Ex: Lists

--- 10_Colecoes/Program.cs
+++ 10_Colecoes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,6 +37,79 @@ namespace _10_Colecoes
             {
                 aluno.ImprimirAluno();
             }
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Impressão dos dicNomes");
+            Dictionary<int,String> dicNomes = new Dictionary<int, string>();
+            dicNomes.Add(1, "Suely");
+            dicNomes.Add(2, "Celso");
+            dicNomes.Add(3, "Gustavo");
+
+            //dicNomes.Add(3, "Gustavo"); // nao permiti chaves duplicadas no Dictionary
+
+            
+            for (int i = 1; i <= dicNomes.Count; i++)
+            {
+                Console.WriteLine($"- {dicNomes[i]}");
+            }
+
+            Console.WriteLine("\nImpressão dos dicAlunos");
+
+            Dictionary<int,Aluno> dicAlunos = new Dictionary<int, Aluno>();
+            dicAlunos.Add(suely.id, suely);
+            dicAlunos.Add(celso.id, celso);
+            /*
+            for(int i = 1; i <= dicAlunos.Count; i++)
+            {
+                dicAlunos[i].ImprimirAluno(); // aqui esta puxando o objeto aluno completo
+            }*/
+
+            foreach(Aluno aluno in dicAlunos.Values) // aqui esta puxando o objeto aluno completo
+            {
+                aluno.ImprimirAluno();
+            }
+
+
+            Queue<String> filaNomes = new Queue<String>(); // FIFO - First In First Out (Primeiro a entrar, primeiro a sair)
+            filaNomes.Enqueue("Suely");
+            filaNomes.Enqueue("Celso");
+            filaNomes.Enqueue("Gustavo");
+
+            Console.WriteLine("\nImpressão dos Queue");
+            Console.WriteLine($" 1°) {filaNomes.Dequeue()}"); // remove o primeiro da fila e retorna o valor
+            foreach (String nome in filaNomes)
+            {
+                Console.WriteLine($"- {nome}");
+            }
+
+            Stack<String> stackNomes = new Stack<string>(); // LIFO - Last In First Out (Ultimo a entrar, primeiro a sair)
+            stackNomes.Push("Suely");
+            stackNomes.Push("Celso");
+            stackNomes.Push("Gustavo");
+
+            Console.WriteLine("\nImpressão dos Stack");
+            Console.WriteLine($" 1°) {stackNomes.Pop()}");
+            foreach (String nome in stackNomes)
+            {
+                Console.WriteLine($"- {nome}");
+            }
+
+            HashSet<String> hashNomes = new HashSet<string>(); // nao permite valores duplicados
+            hashNomes.Add("Suely");
+            hashNomes.Add("Celso");
+            hashNomes.Add("Gustavo");
+
+            // Ignora valores duplicados
+            hashNomes.Add("Gustavo"); // nao vai adicionar pois ja existe na lista
+
+            Console.WriteLine("\nImpressão dos HashSet");
+            foreach (String nome in hashNomes)
+            {
+                Console.WriteLine($"- {nome}");
+            }
+
+
         }
     }
 
