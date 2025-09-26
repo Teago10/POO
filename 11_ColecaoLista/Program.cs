aa6ab01faed6736c326305aefef686033f195f35
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-26 19:23:51 -0300

    Criação da Lista de armazenamento de Nomes

---
+++ 11_ColecaoLista/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_ColecaoLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe nomes de pessoas");
+            List<string> listNome = new List<string>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"Digite o {i}° Nome: ");
+                listNome.Add(Console.ReadLine());
+                
+            }
+
+            foreach (string nome in listNome)
+            {
+                Console.WriteLine($"Nome: {nome}");
+            }
+        }
+    }
+}
