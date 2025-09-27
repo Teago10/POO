0363fda1a4a27e6d1145c362579d7245f0c414f2
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-26 21:06:16 -0300

    Criação da Stack

---
+++ 13_ColecaoPilha/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_ColecaoPilha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stackNum = new Stack<int>();
+            stackNum.Push(1);
+            stackNum.Push(2);
+            stackNum.Push(3);
+            stackNum.Push(4);
+            stackNum.Push(5);
+            stackNum.Push(6);
+            stackNum.Push(7);
+
+            Console.WriteLine($" 1°) {stackNum.Pop()}");
+        }
+    }
+}
