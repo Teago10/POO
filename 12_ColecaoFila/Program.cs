13a0b63a38192a0e14b7587c073f2cf110abb0a8
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-26 21:02:39 -0300

    Criação da Queue

---
+++ 12_ColecaoFila/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_ColecaoFila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> filaNome = new Queue<string>();
+
+            Console.WriteLine("Digite o nome das Pessoas  (mínimo de 3 caracteres)");
+            int contador = 0;
+            while (contador < 4)
+            {
+                Console.WriteLine("Informe o nome: ");
+                string nome = Console.ReadLine();
+                
+                if (nome.Length >= 3)
+                {
+                    filaNome.Enqueue(nome);
+                    contador++;
+                }
+                else
+                {
+                    Console.WriteLine("Nome muito curto");
+                }
+            }
+
+            int i = 1;
+            foreach (string nome in filaNome)
+            {
+                if (i <= 3)
+                    Console.WriteLine($"{i}° Nome: {nome}");
+                    i++;
+            }
+        }
+    }
+}
