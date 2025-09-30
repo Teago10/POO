22bf17125aa54006d32bd437173488a6f4b00d0c
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-09-29 22:18:03 -0300

    Atualização utilizando o Enum

---
+++ 17_Calendario/Feriados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_Calendario
+{
+    internal class Feriado
+    {
+        public int Dia { get; set; } // dia do feriado
+        public string Descricao { get; set; } // descrição do feriado
+        public Feriado(int dia, string descricao) // construtor
+        {
+            this.Dia = dia;
+            this.Descricao = descricao;
+        }
+    }
+}
