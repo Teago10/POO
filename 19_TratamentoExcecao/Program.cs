96f640cc37087e2e2d7d9f7bd638b7a847d51d16
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-10-06 20:42:24 -0300

    Tratamento de Exceção

---
+++ 19_TratamentoExcecao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_TratamentoExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            while (true)
+            {
+                Console.WriteLine("Digite um numero inteiro: ");
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine("Número inválido, informe apenas numeros inteiros!.\nPressione qualquer tecla para continuar");
+                    Console.ReadKey();
+                    continue;
+
+                }
+                finally //sempre executa
+                {
+                    Console.Clear();
+                }
+            }
+
+            if(numero % 2 == 0)
+                Console.WriteLine("Número par!");
+            else
+                Console.WriteLine("Número ímpar!");
+        }
+    }
+}
