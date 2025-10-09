998dd242cdd0702c8e8d71bfaf7689a442b9777c
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-10-09 20:18:11 -0300

    Criação do código de Divisão utilizando exceções

---
+++ 22_DivisaoComExcecao/Calculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_DivisaoComExcecao
+{
+    internal class Calculo
+    {
+        private int numero1 { get; }
+        private int numero2 { get; }
+
+        public Calculo(int numero1, int numero2) 
+        { 
+            this.numero1 = numero1;
+            this.numero2 = numero2;
+        }
+
+        public int GetNumero() 
+        {
+            return numero1 / numero2;
+        }
+    }
+}
---
+++ 22_DivisaoComExcecao/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22_DivisaoComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Digite um numero inteiro:");
+                    numero1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite outro numero inteiro:");
+                    numero2 = int.Parse(Console.ReadLine());
+
+                    Calculo calculo = new Calculo(numero1, numero2);
+                    Console.WriteLine($"A divisão entre eles é {calculo.GetNumero()}");
+                    break;
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Divisão por zero será sempre zero");
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Valor inválido. Digite um numero inteiro");
+                    Console.WriteLine(ex.Message);
+                }
+                finally 
+                {
+                    Console.WriteLine("Operação finalizada. Obrigado por utilizar o programa!");
+                }
+            }
+        }
+    }
+}
