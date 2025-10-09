b061a74a41a8b706825c36b0001c7c4db2088ae0
Thiago Affonso <thiagoaffonso452@gmail.com>
2025-10-09 19:56:55 -0300

    Criação do Código Conversão de Numero Com Excecao

---
+++ 21_ConversaoNumeroComExcecao/Calculo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_ConversaoNumeroComExcecao
+{
+    internal class Calculo
+    {
+        private int codigo { get; }
+
+        public Calculo(int codigo)
+        {
+            this.codigo = codigo;
+        }
+        public int Dobro()
+        {
+            return codigo * 2;
+        }
+    }
+}
---
+++ 21_ConversaoNumeroComExcecao/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_ConversaoNumeroComExcecao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            while (true) 
+            {
+                Console.WriteLine("Digite um numero inteiro: ");
+                
+
+                try
+                {
+                    numero = int.Parse(Console.ReadLine());
+                    Calculo calculo = new Calculo(numero);
+                    Console.WriteLine($"O dobro do numero é {calculo.Dobro()}");
+                    break;
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Valor inválido! Digite apenas números inteiros.");
+                    Console.WriteLine(ex.Message);
+                }
+
+                
+            }
+        }
+    }
+}
